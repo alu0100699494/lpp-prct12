4f45048b9e07a869e2180752b41ab12c63181a4e
Daniel Herzog <alu0100699494@ull.edu.es>
2013-11-13 16:31:32 +0000

    Expectativas de la clase Matriz_Densa recicladas de la práctica 8. Modificados y eliminados algunos puntos.

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -18,7 +18,39 @@ describe MathExpansion::Matriz do
 end
 
 describe MathExpansion::Matriz_Densa do
-
+	before :each do
+		@m1 = MathExpansion::Matriz_Densa.new(2,2)
+		@m2 = MathExpansion::Matriz_Densa.new(2,2)
+		
+		@m1.set(0,0).should eq(1)
+		@m1.set(0,1).should eq(2)
+		@m1.set(1,0).should eq(3)
+		@m1.set(1,1).should eq(4)
+		
+		@m2.set(0,0).should eq(2)
+		@m2.set(0,1).should eq(4)
+		@m2.set(1,0).should eq(6)
+		@m2.set(1,1).should eq(8)
+		
+	end
+	describe " # Almacenamiento de matrices. " do
+		it " # Se debe poder acceder a los datos almacenados en la matriz " do		
+			@m1.get(0,0).should eq(1)
+			@m1.get(0,1).should eq(3)
+		end
+		it " # Se deben poder modificar los datos almacenados en la matriz " do		
+			@m1.set(0,0,5)
+			@m1.get(0,0).should eq(5)
+			
+			@m1.set(0,1,8)
+			@m1.get(0,1).should eq(8)
+		end
+		it " # Se deben poder almacenar todo tipo de datos numericos (flotantes, enteros, etc...) " do		
+			@m1.set(0,0,3.0)
+			@m1.set(0,1,-6)
+			@m1.to_s.should == "3.0\t2\t\n-6\t4\t\n"
+		end
+	end
 end
 
 
