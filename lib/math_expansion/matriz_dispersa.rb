9d6444b7d68a9a0eb6b8f7556607a1f06fa03f54
Daniel Herzog <alu0100699494@ull.edu.es>
2013-11-13 20:41:43 +0000

    Simplificada la clase Matriz_Dispersa para trabajar con un Array de Hashes en vez de con un Array de Arrays. Ahora deberá funcionar de una manera más simple y legible. Para cada fila de la matriz, se almacenará un Hash, cuya clave representará la columna de un elemento, y el valor, el contenido o valor del elemento de la matriz en esa fila dada por el Array y por la columna dada por la clave del Hash. Así, se gestiona de forma simple que no haya claves repetidas, ordenándolas de manera correcta, etc.

--- lib/math_expansion/matriz_dispersa.rb
+++ lib/math_expansion/matriz_dispersa.rb
@@ -10,17 +10,13 @@ module MathExpansion
 	    @columna, @valor = columna, valor
 	  end
 	end
-	#@contenido guarda los valores no nulos.
-	#@indice_contenido guarda la fila del valor no nulo correspondiente a @contenido.
-	#@comienzo_columna guarda la posición de comienzo de la columna.
-	#@elementos_columna es un vector que contiene el numero de elementos de la columna.
-	#@n_nonulo: número de valores no nulos.
+	# Nota: el contenido se podria representar de forma mas sencilla con un Array de Hashes, y no con un Array de Arrays
 	
 	def reset
 	  @contenido = Array.new(@N) # Array con @N filas y ninguna columna (vacio)
 	  i = 0
 	  while(i < @N)
-	    @contenido[i] = []
+	    @contenido[i] = {}
 		i += 1
 	  end
 	end
@@ -43,7 +39,7 @@ module MathExpansion
 		  raise RuntimeError , "No se ha definido \"null\" para la clase #{value.class}" unless value.class.respond_to? :null
 		  
 		  if( value != value.class.null)
-		    obj.contenido[i] << Elemento.new(j, value)
+		    obj.contenido[i][j] = value
 		  end #endif
 		  j += 1
 		end #endwhile j
@@ -67,18 +63,15 @@ module MathExpansion
 	end #endmethod null_percent
 	
 	def get(i, j)
-	  if( i < 0 or i >=@N or j < 0 or j >= @M)
+	  if( !(i.is_a? Fixnum) or i < 0 or i >=@N or !(j.is_a? Fixnum) or j < 0 or j >= @M)
 	    return nil
 	  end
-	  
-	  k = 0
-	  while( k < @contenido[i].size and @contenido[i].size > 0)
-	    if(@contenido[i][k].columna == j)
-		  return @contenido[i][k].valor
-		end
-	    k += 1
+	    
+	  if(@contenido[i][j] != nil) # Elemento no nulo (esta en el hash)
+	    return @contenido[i][j]
+	  else # Elemento nulo (no esta en el Hash)
+	    return 0
 	  end
-	  return 0
 	end #endmethod get
 	
 	def set(i, j, value)
@@ -87,34 +80,19 @@ module MathExpansion
 		return nil
 	  end
 	  
-	  if( i < 0 or i >=@N or j < 0 or j >= @M or value == value.class.null)
-	    puts "Elemento fuera de rango o de valor nulo."
+	  if( !(i.is_a? Fixnum) or i < 0 or i >=@N or !(j.is_a? Fixnum) or j < 0 or j >= @M)
 	    return nil
 	  end
 	  
-	  k = 0
-	  # COMPROBAR VALORES NULOS!
-	  if(@contenido[i].size == 0)
-	    @contenido[i].insert(0, Elemento.new(j, value))
-      else
-  	    while( k < @contenido[i].size and @contenido[i].size > 0 and j > @contenido[i][k].columna)
-	      k += 1
-	    end
+	  if(value == nil or value == value.class.null)
+	    @contenido[i].delete(j) # Borrar elemento (valor nulo)
+	  else
+	    @contenido[i][j] = value
+	  end
 	  
-	    # 2 casos:
-		if(k == @contenido[i].size) # Ultimo elemento del vector
-		  @contenido[i] << Elemento.new(j, value)
-		else
-	      if(j == @contenido[i][k].columna)    # j == @contenido[i][k].column
-	        @contenido[i][k].valor = value
-	      elsif(j < @contenido[i][k].columna)  # j  < @contenido[i][k].column
-	        @contenido[i].insert(k, Elemento.new(j, value))
-	      end
-		end
-	  end #endif
 	  if(null_percent < 0.6) # Si se ha sobrepasado el número de elementos nulos, borramos el último elemento modificado
-		  @contenido[i].delete_at(k)
-		  puts "Borrado el elemento #{i},#{k} por sobrepasar el número de elementos no nulos (Porcentaje actual: #{null_percent}"
+		  @contenido[i].delete(j)
+		  puts "Borrado el elemento #{i},#{j} por sobrepasar el número de elementos no nulos (Porcentaje actual: #{null_percent}"
 	  end
 	  
 	end #endmethod set
