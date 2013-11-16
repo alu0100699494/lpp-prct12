2b45069c77f6793bdf32f50ea6346af421797dc7
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-11-16 18:07:02 +0000

    Implementadas operaciones para matrices. Creadas expectativas. Ejecucion testeada y correcta.

--- lib/math_expansion/matriz_densa.rb
+++ lib/math_expansion/matriz_densa.rb
@@ -54,5 +54,78 @@ module MathExpansion
       s
     end
     
+    def +(other)
+      		raise ArgumentError , 'Tipo invalido' unless other.is_a? Matriz
+      		raise ArgumentError , 'Matriz no compatible' unless @N == other.N and @M == other.M
+  
+      		c = Matriz_Densa.new(@N, @M)
+      		i = 0
+		while(i < @N)
+        		j = 0
+        		while(j < @M)
+	      			c.set(i, j, get(i,j) + other.get(i,j))
+    	  			j += 1
+    			end 
+    			i += 1
+      		end
+      		c
+    end
+	
+    def -(other)
+      		raise ArgumentError , 'Tipo invalido' unless other.is_a? Matriz
+      		raise ArgumentError , 'Matriz no compatible' unless @N == other.N and @M == other.M
+  
+      		c = Matriz_Densa.new(@N, @M)
+      		i = 0
+      		while(i < @N)
+        		j = 0
+        		while(j < @M)
+	      			c.set(i, j, get(i,j) - other.get(i,j))
+    	  			j += 1
+    			end
+    			i += 1
+      		end
+      		c
+    end
+	
+    def *(other)
+    		raise ArgumentError , 'Parametro invalido' unless other.is_a? Numeric or other.is_a? Matriz
+
+    		if(other.is_a? Numeric) # Matriz * numero
+      			c = Matriz_Densa.new(@N, @M)
+	  		i = 0
+      			while(i < @N)
+        			j = 0
+        			while(j < @M)
+         				c.set(i, j, get(i,j)*other)
+         				j += 1
+	    			end # while j
+	    			i += 1
+      			end # while i
+    		else # Matriz * Matriz
+      			raise ArgumentError , 'Matriz no compatible (A.N == B.M)' unless @M == other.N
+      			c = Matriz_Densa.new(@N, other.M)
+      			i = 0
+      			while(i < @N)
+        			j = 0
+        			while(j < other.M)
+	      				k = 0
+					#if (get(i,j).is_a? Fraccion)	      				
+					#	c.set(i, j, Fraccion.null
+					#else
+					#	c.set(i, j, 0)
+					#end
+	      				while(k < @M)
+	        				c.set(i, j, get(i, k) * other.get(k,j) + c.get(i,j))
+	        				k += 1
+	      				end # while k
+          				j += 1
+        			end # while j
+	    			i += 1
+      			end # while i
+    		end # while else
+  
+    		c
+    end # *(other)
   end # Class
 end # Module
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -73,6 +73,37 @@ describe MathExpansion::Matriz_Densa do
             @m1.to_s.should == "3.0\t-6\t\n3\t4\t\n"
         end
     end
+
+    describe " # Operaciones con matrices densas. " do
+	it " # Se debe poder sumar dos matrices " do
+		@m3 = MathExpansion::Matriz_Densa.new(2,2)
+		@m3.set(0,0,6)
+		@m3.set(0,1,8)
+		@m3.set(1,0,10)
+		@m3.set(1,1,12)
+
+		(@m1+@m2).to_s.should eq(@m3.to_s)
+	end
+
+	it " # Se debe poder restar dos matrices " do
+		@m3 = MathExpansion::Matriz_Densa.new(2,2)
+		@m3.set(0,0,4)
+		@m3.set(0,1,4)
+		@m3.set(1,0,4)
+		@m3.set(1,1,4)
+
+		(@m2-@m1).to_s.should eq(@m3.to_s)
+	end
+	it " # Se debe poder multiplicar dos matrices " do
+		@m3 = MathExpansion::Matriz_Densa.new(2,2)
+		@m3.set(0,0,19)
+		@m3.set(0,1,22)
+		@m3.set(1,0,43)
+		@m3.set(1,1,50)
+
+		(@m1*@m2).to_s.should eq(@m3.to_s)
+	end
+    end
 end
 
 
@@ -162,5 +193,53 @@ describe MathExpansion::Matriz_Dispersa do
             @md1.to_s.should == "Fila 0: \nFila 1: 0=>1 1=>3 \nFila 2: \n"
             @md2.to_s.should == "Fila 0: \nFila 1: \nFila 2: \n"
         end
+
+	
+    end
+
+    describe " # Operaciones con matrices dispersas. " do
+	it " # Se debe poder sumar dos matrices " do		
+		@m3 = MathExpansion::Matriz_Densa.new(3,2)
+		@m3.set(0,0,0)
+		@m3.set(0,1,0)
+		@m3.set(1,0,1)
+		@m3.set(1,1,3)
+		@m3.set(2,0,0)
+		@m3.set(2,1,0)
+		
+		
+		(@md1+@md2).to_s.should eq(@m3.to_s)
+	end
+	it " # Se debe poder restar dos matrices " do
+		@m3 = MathExpansion::Matriz_Densa.new(3,2)
+		@m3.set(0,0,0)
+		@m3.set(0,1,0)
+		@m3.set(1,0,-1)
+		@m3.set(1,1,-3)
+		@m3.set(2,0,0)
+		@m3.set(2,1,0)
+
+		(@md2-@md1).to_s.should eq(@m3.to_s)
+	end
+	it " # Se debe poder multiplicar dos matrices " do
+		@md4 = MathExpansion::Matriz_Dispersa.new(2,3)
+		@md4.set(1,0,-1)
+		@md4.set(1,1,-1)
+		@md4.set(1,2,0)
+		
+		@m3 = MathExpansion::Matriz_Densa.new(3,3)
+		@m3.set(0,0,0)
+		@m3.set(0,1,0)
+		@m3.set(0,2,0)
+		@m3.set(1,0,-3)
+		@m3.set(1,1,-3)
+		@m3.set(1,2,0)
+		@m3.set(2,0,0)
+		@m3.set(2,1,0)
+		@m3.set(2,2,0)
+
+		(@md1*@md4).to_s.should eq(@m3.to_s)
+	end
+
     end
 end
