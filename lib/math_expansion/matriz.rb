914101ee7b33db50ab72e7644682296010813162
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-11-20 12:30:10 +0000

    Corregido accessor a contenido en matriz.rb

--- lib/math_expansion/matriz.rb
+++ lib/math_expansion/matriz.rb
@@ -3,8 +3,8 @@ require './lib/math_expansion/racional.rb'
 module MathExpansion
   class Matriz
     attr_reader :N, :M
-    attr_accessor :contenido
-    
+    attr_reader :contenido
+ 
     def initialize(n, m)
       raise ArgumentError, 'Indice no valido' unless n.is_a? Fixnum and n > 0 and m.is_a? Fixnum and m > 0
     
